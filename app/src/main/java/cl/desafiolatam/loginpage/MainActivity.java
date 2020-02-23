cc9ed6e2c9177c9b082c09f62472e60568b720ce
Alejandro Perez <alejandro8001@gmail.com>
2020-02-23 15:49:07 -0300

    provando cosas

--- app/src/main/java/cl/desafiolatam/loginpage/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/loginpage/MainActivity.java
@@ -2,6 +2,7 @@ package cl.desafiolatam.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,27 +11,32 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
-public ImageView imageView;
+    public ImageView imageView;
 
-private Button MiButton;
+    private Button MiButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView=findViewById(R.id.imageView);
-        Picasso.get (). load ("http://blog.desafiolatam.com/wp-content/uploads/2019/02/logo.png") .into (imageView);
+        imageView = findViewById(R.id.imageView);
+        Picasso.get().load("http://blog.desafiolatam.com/wp-content/uploads/2019/02/logo.png").into(imageView);
 
-    Button btntoast = (Button)findViewById(R.id.button);
-    btntoast.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "Usuario ingreso con exito", Toast.LENGTH_SHORT).show();
-        }
-    });
+        Button btntoast = (Button) findViewById(R.id.button);
+        btntoast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Usuario ingreso con exito", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+
+        
 
     }
 
---
+++ app/src/main/java/cl/desafiolatam/loginpage/RegistroUsuario.java
@@ -0,0 +1,4 @@
+package cl.desafiolatam.loginpage;
+
+public class RegistroUsuario {
+}
