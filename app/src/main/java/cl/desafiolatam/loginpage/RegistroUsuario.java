8ffbf1fe637703a939102ab14bb144ec82b15f86
Alejandro Perez <alejandro8001@gmail.com>
2020-02-26 15:25:55 -0300

    desafio parte 2 con cambio en radio group 2.2

--- app/src/main/java/cl/desafiolatam/loginpage/RegistroUsuario.java
+++ app/src/main/java/cl/desafiolatam/loginpage/RegistroUsuario.java
@@ -1,6 +1,9 @@
 package cl.desafiolatam.loginpage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,20 @@ public class RegistroUsuario extends AppCompatActivity {
 
 
 
-
+    Button button3;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingreso_usuario);
+        button3 = (Button) findViewById(R.id.button3);
 
+        button3.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(getApplicationContext(), "Usuario ingreso botton 2", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RegistroUsuario.this, ActivitiHome.class));
+            }
+        });
 
 
 
